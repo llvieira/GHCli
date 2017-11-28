d064e5f0b23cd4cdab48377b3590c2a98696b8a8
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-11-28 08:44:31 -0300

    # This is a combination of 19 commits. # This is the 1st commit message:
    Resolving conflict
    
    # This is the commit message #2:
    
    Conflict resolved
    
    # This is the commit message #3:
    
    add navbar required lib
    
    # This is the commit message #4:
    
    fix AS3.0 bug with firebase-plugin1.1.1
    
    # This is the commit message #5:
    
    add navbar dependencies
    
    # This is the commit message #6:
    
    add navbar to activity layout
    
    # This is the commit message #7:
    
    strings for navbar's menu
    
    # This is the commit message #8:
    
    add navbar options @TODO add icons
    
    # This is the commit message #9:
    
    add navbar button action
    
    # This is the commit message #10:
    
    add select fragment method
    
    # This is the commit message #11:
    
    fix import errors; adaptations of methods;
    
    # This is the commit message #12:
    
    fixing imports and method calls
    
    # This is the commit message #13:
    
    change home button to profile
    
    # This is the commit message #14:
    
    fix last errors
    
    # This is the commit message #15:
    
    add profile fragment
    
    # This is the commit message #16:
    
    add followers fragment
    
    # This is the commit message #17:
    
    add framents xml
    
    # This is the commit message #18:
    
    add dummie instanciation of fragments
    
    # This is the commit message #19:
    
    Fixed problem in fragments

--- app/src/main/java/com/github/ghcli/activities/HomePage.java
+++ app/src/main/java/com/github/ghcli/activities/HomePage.java
@@ -1,18 +1,81 @@
 package com.github.ghcli.activities;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.MenuItem;
+
+
 
 import com.github.ghcli.R;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener, FollowersFragment.OnFragmentInteractionListener {
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+
+    private BottomNavigationView navBar;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        navBar = (BottomNavigationView) findViewById(R.id.navigation);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+
+        MenuItem selectedItem;
+
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = navBar.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = navBar.getMenu().getItem(1);
+        }
+        selectFragment(selectedItem);
     }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.menu_profile:
+                frag = ProfileFragment.newInstance("test1","test2");
+                break;
+            case R.id.menu_followers:
+                frag = FollowersFragment.newInstance("teste1","teste2");
+                break;
+        }
+
+        // update selected item
+        mSelectedItem = item.getItemId();
+
+        // uncheck the other items.
+        for (int i = 0; i< navBar.getMenu().size(); i++) {
+            MenuItem menuItem = navBar.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.activity_home_page, frag, frag.getTag());
+            ft.commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+    }
+    //heavily based on https://github.com/segunfamisa/bottom-navigation-demo/blob/master/app/src/main/java/com/segunfamisa/sample/bottomnav/MainActivity.java
 }
