510bba3aac23345818883ea47ae41d17e29403a9
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2018-02-13 16:33:48 -0300

    adjustments in comments and logs

--- app/src/main/java/com/github/ghcli/interfaces/RecyclerViewOnClickListener.java
+++ app/src/main/java/com/github/ghcli/interfaces/RecyclerViewOnClickListener.java
@@ -2,10 +2,6 @@ package com.github.ghcli.interfaces;
 
 import android.view.View;
 
-/**
- * Created by pedro on 26/12/2016.
- */
-
 public interface RecyclerViewOnClickListener {
 
     public void onClickListener(View view, int position);
